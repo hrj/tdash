05d22afac26f9334d4f5c7dbcfe28092cbb149ed
Brandon Williams <rocketeerbkw@gmail.com>
2013-08-11 21:41:41 -0700

    Increase java stack size for `sbt package` to work

--- framework/sbt.sh
+++ framework/sbt.sh
@@ -3,4 +3,4 @@ if [ -z "$BIN_DIR" ]; then
 fi
 
 # scala -cp tools/sbt_0.5.2_for_2.7.5.jar sbt.Main $*
-java -jar ${BIN_DIR}/sbt-launch_0.10.jar $*
+java -Xss2M -jar ${BIN_DIR}/sbt-launch_0.10.jar $*
